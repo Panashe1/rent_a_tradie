e345891ae04cab52bf9b57227b296a77d9377917
z-pl <60358608+z-pl@users.noreply.github.com>
2022-06-19 15:37:14 +1000

    Model and controller config (#8)
    * services controller
    
    * created service seed

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,26 +1,48 @@
 class ServicesController < ApplicationController
   def index
+    @services = Service.all
   end
 
   def new
+    @service = Service.new
   end
 
   def create
+    @service = Service.new(service_params)
+
+    if @service.save
+      redirect_to services_path
+    else
+      render :new
+    end
   end
 
   def show
+    find_service
   end
 
   def edit
+    find_service
   end
 
   def update
+    find_service
+    @service.update(service_params);
   end
 
   def destroy
+    find_service
+    @service.destroy
+    redirect_to services_path
   end
 
   private
 
+  def service_params
+    params.require(:service).permit(:category, :description, :cost, :average_rating, :title, :user_id)
+  end
 
+  def find_service
+    @service = Service.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating seeds"
+Service.destroy_all
+15.times do
+  Service.create(
+    title: Faker::Company.name,
+    description: Faker::Marketing.buzzwords,
+    cost: "#{rand(1..300)}",
+    average_rating: rand(1..5),
+    category: Faker::Job.field,
+    user_id: 1)
+end
+
+puts "Finished creating seeds"
