b6d57af7a42a910e503f75da8ededb2952239e8f
Josh Smith <josh@jshsmth.com>
2022-06-19 15:55:53 +1000

    minor change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @services = Service.all
   end
 end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,7 +1,4 @@
 class ServicesController < ApplicationController
-  def index
-    @services = Service.all
-  end
 
   def new
     @service = Service.new
