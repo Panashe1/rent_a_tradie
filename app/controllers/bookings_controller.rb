bcc043b7f046cb4966d3811b5704d805a8da7dae
Josh Smith <josh@jshsmth.com>
2022-06-20 02:40:10 -0700

    change show page (#12)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,19 @@
 class BookingsController < ApplicationController
   def new
+    @service = Service.find(params[:service_id])
+    @user = User.find(params[:user_id])
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @service = Service.find(params[:service_id])
+    @booking.service = @service
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
