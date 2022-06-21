48a899be53c82959c6fb7a8ece8beaccb3f8ced6
Josh Smith <josh@jshsmth.com>
2022-06-21 22:51:03 +1000

    chore: minor buttons

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -21,6 +21,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :comment, :user_id)
+    params.require(:booking).permit(:start_date, :end_date, :comments, :user_id, :service_id)
   end
 end
