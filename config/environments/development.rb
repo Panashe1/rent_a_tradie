ebde1f712ba590d7f51502324cd75e952bb17cd1
Josh Smith <josh@jshsmth.com>
2022-06-21 02:20:36 -0700

    added cloud image hosting (#17)
    * added cloud image hosting
    
    * added cloud image hosting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Image hosting
+  config.active_storage.service = :cloudinary
   # Show full error reports.
   config.consider_all_requests_local = true
 
