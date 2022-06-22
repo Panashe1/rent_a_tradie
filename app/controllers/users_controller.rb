9c942635e97b5382e9aaff55d60a184dc0bba8ef
Josh Smith <josh@jshsmth.com>
2022-06-22 02:44:17 -0700

    Create user profile page (#27)
    * added some simple validations to models
    
    * template profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @bookings = Booking.all
+    @services = Service.all
   end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,8 @@
 class Service < ApplicationRecord
+  validates :category, presence: true
+  validates :description, presence: true
+  validates :cost, presence: true
+  validates :title, presence: true
   has_one_attached :photo
   belongs_to :user
   has_many :bookings
