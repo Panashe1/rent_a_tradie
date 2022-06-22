407405332803a878953f7b3893f87ddc8a5bd16b
Josh Smith <josh@jshsmth.com>
2022-06-22 18:48:17 +1000

    added some simple validations to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :comments, presence: true
   belongs_to :service
   belongs_to :user
 end
