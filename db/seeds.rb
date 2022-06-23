653ed2a941599f4522f1ebb5086b411717096bf5
z-pl <60358608+z-pl@users.noreply.github.com>
2022-06-23 20:02:44 +1000

    changed seed file to add name and services (#32)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,17 @@
 puts "Creating seeds"
 Service.destroy_all
 
+services = ["Electrician", "Plumber", "Gardener", "Woodwork", "Painter", "Handy Man", "Mover"];
 User.create(
   email: "test@gmail.com",
   password: 'password')
 15.times do
   Service.create(
-    title: Faker::Company.name,
+    title: Faker::Name.name,
     description: Faker::Marketing.buzzwords,
     cost: "#{rand(1..300)}",
     average_rating: rand(1..5),
-    category: Faker::Job.field,
+    category: services.sample,
     user_id: 1)
 end
 
