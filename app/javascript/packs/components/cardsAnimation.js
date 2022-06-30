232b995b2084e95d404915bedcf46a8da7160eaf
z-pl <60358608+z-pl@users.noreply.github.com>
2022-06-30 18:44:13 +1000

    added fade animation and colors to cards (#51)
    * added fade animation and colors to cards
    
    * cards animation

---
+++ app/javascript/packs/components/cardsAnimation.js
@@ -0,0 +1,52 @@
+
+const faders = document.querySelectorAll('.fade-in');
+const numOfCols = Math.floor((document.documentElement.clientWidth  / 280) - 0.19); // how the fuck did I end up calculating this?
+const transitionDelay = 0.2;
+
+let entryCount = 1;
+let totalCount = 0;
+const appearOptions = {
+  threshold: 0
+  //rootMargin: "0px 0px -10px 0px"
+};
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+  console.log("function call");
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      return
+    } else {
+      entry.target.style.transitionDelay = `${entryCount * transitionDelay}s` ;
+      entry.target.classList.add('is-visible');
+      appearOnScroll.unobserve(entry.target);
+
+      entryCount++;
+      totalCount++;
+
+      if (entryCount > numOfCols) {
+        entryCount = 1;
+      }
+    }
+  })
+
+  if (totalCount === faders.length) {
+    setTimeout(() => {
+      const visibleFaders = document.querySelectorAll(".fade-in")
+      visibleFaders.forEach(fader => {
+        fader.style.transitionDelay = `0s`;
+        fader.classList.toggle('scale-card')
+      })
+    }, 1000);
+  }
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
+
+export {
+  appearOnScroll
+}
+
+/*
+
+*/
