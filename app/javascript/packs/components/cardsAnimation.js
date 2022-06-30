af56961164367ca7e4d8925ce06f5f43dfed6f5d
Josh Smith <josh@jshsmth.com>
2022-06-30 18:56:43 +1000

    js-fix

--- app/javascript/packs/components/cardsAnimation.js
+++ app/javascript/packs/components/cardsAnimation.js
@@ -1,22 +1,21 @@
-
-const faders = document.querySelectorAll('.fade-in');
-const numOfCols = Math.floor((document.documentElement.clientWidth  / 280) - 0.19); // how the fuck did I end up calculating this?
+const faders = document.querySelectorAll(".fade-in");
+const numOfCols = Math.floor(document.documentElement.clientWidth / 280 - 0.19); // how the fuck did I end up calculating this?
 const transitionDelay = 0.2;
+const refreshLinks = document.querySelectorAll(".js-refresh");
 
 let entryCount = 1;
 let totalCount = 0;
 const appearOptions = {
-  threshold: 0
+  threshold: 0,
   //rootMargin: "0px 0px -10px 0px"
 };
 const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
-  console.log("function call");
-  entries.forEach(entry => {
-    if(!entry.isIntersecting) {
-      return
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
     } else {
-      entry.target.style.transitionDelay = `${entryCount * transitionDelay}s` ;
-      entry.target.classList.add('is-visible');
+      entry.target.style.transitionDelay = `${entryCount * transitionDelay}s`;
+      entry.target.classList.add("is-visible");
       appearOnScroll.unobserve(entry.target);
 
       entryCount++;
@@ -26,26 +25,30 @@ const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
         entryCount = 1;
       }
     }
-  })
+  });
+
+  refreshLinks.forEach((link) => {
+    return link.addEventListener("click", () => {
+      location.reload();
+    });
+  });
 
   if (totalCount === faders.length) {
     setTimeout(() => {
-      const visibleFaders = document.querySelectorAll(".fade-in")
-      visibleFaders.forEach(fader => {
+      const visibleFaders = document.querySelectorAll(".fade-in");
+      visibleFaders.forEach((fader) => {
         fader.style.transitionDelay = `0s`;
-        fader.classList.toggle('scale-card')
-      })
+        fader.classList.toggle("scale-card");
+      });
     }, 1000);
   }
 }, appearOptions);
 
-faders.forEach(fader => {
+faders.forEach((fader) => {
   appearOnScroll.observe(fader);
-})
+});
 
-export {
-  appearOnScroll
-}
+export { appearOnScroll };
 
 /*
 
